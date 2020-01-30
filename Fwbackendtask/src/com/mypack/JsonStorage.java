41c9fee6ce24b87402c678b7aeecf4a0f8d917d9
runMan <runMan@r>
2020-01-31 03:46:06 +0530

    formatted code has been added

--- Fwbackendtask/src/com/mypack/JsonStorage.java
+++ Fwbackendtask/src/com/mypack/JsonStorage.java
@@ -15,85 +15,47 @@ public class JsonStorage {
 	private static final int TTLinterval = 300; // 5 minutes
 	private static final long defaultFileSize = 1073741824; // 1Gb
 	private final static String defaultFilePath = "c:\\FW-JsonStorage";
-	
+
 	String currentPath = "";
+
 	public JsonStorage(String optionalPath) {
 		this.checkAndCreateFilePath(optionalPath);
 	}
+
 	public ObjectMapper getOMObject() {
 		return new ObjectMapper();
 	}
 
-	public synchronized String delete(String key) {
-		String log = "";
+	private String checkAndCreateFilePath(String optionalPath) {
+
 		try {
-			if (!this.currentPath.equals("")) {
-				File sourceFile = new File(this.currentPath);
-				if (sourceFile.exists()) {
-					JsonNode root = mapper.readTree(sourceFile);
-					if (root != null) {
-						if (root.get(key) != null) {
-							if (checkTTLProperty(key, root)) {
-								log = "Error :This key has expired,this key will no longer be available to Delete operations";
-							} else {
-								((ObjectNode) root).remove(key);
-								log = "Info: " + key + " key & values has been deleted successfully";
-								mapper.writeValue(sourceFile, root);
-							}
-						} else {
-							log = "Info: Key is not found";
-						}
+			if (!optionalPath.equals("")) {
+				File path = new File(optionalPath);
+				if (path.exists()) {
+					this.currentPath = optionalPath + "\\local_storage.json";
+				} else {
+					System.out.println("Error: Please Check Your Folder Path: " + optionalPath);
+				}
+			} else {
+				File newFileObj = new File(defaultFilePath);
+				if (newFileObj.exists()) {
+					this.currentPath = defaultFilePath + "\\local_storage.json";
+				} else {
+					if (newFileObj.mkdir()) {
+						this.currentPath = defaultFilePath + "\\local_storage.json";
 					}
 				}
 			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return log;
-	}
-
-	private static Boolean checkTTLProperty(String key, JsonNode root) throws ParseException {
-		Boolean status = false;
-		String ttlValue = ((ObjectNode) root).path(key).path("TTL").asText();
-		SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM d HH:mm:ss zzz yyyy");
-		Date ttl = formatter.parse(ttlValue);
-		Date currentDate = new Date();
-		if (ttl.compareTo(currentDate) < 0) {
-			status = true;
-		}
-		return status;
-	}
-
-	public synchronized String read(String key) {
-		ArrayList<Object> listobj = new ArrayList<>();
-		try {
-			if (!this.currentPath.equals("")) {
-				File sourceFile = new File(this.currentPath);
-				if (sourceFile.exists()) {
-					JsonNode root = mapper.readTree(sourceFile);
-					if (root != null) {
-						if (root.get(key) != null) {
-							if (checkTTLProperty(key, root)) {
-								listobj.add(
-										"Error :This key has expired,the key will no longer be available to Read operations");
-							} else {
-								listobj.add(root.path(key));
-							}
-						} else {
-							listobj.add("Info : Key is not found");
-						}
-					}
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return listobj.get(0).toString();
+		return currentPath;
 	}
 
 	public synchronized String create(String key, ObjectNode values, String... timeToLive) {
 		String log = "";
-		double objectValueSize = getObjetSize(values);
+		double objectValueSize = getObjectSize(values);
 		double sourecFileSize = getSoureFileSize();
 		if (sourecFileSize > defaultFileSize) {
 			log = "Error: you storage was full.please contect your fw-admin Team";
@@ -107,36 +69,7 @@ public class JsonStorage {
 		return log;
 	}
 
-	private static double getObjetSize(ObjectNode values) {
-		double objectValueSize = 0.00;
-		try {
-			File tempFile = File.createTempFile("fw-tempFile", ".tmp");
-			mapper.writeValue(tempFile, values);
-			double kilobytes = ((double) tempFile.length() / 1024);
-			objectValueSize = Math.round(kilobytes);
-			// System.out.println("Info: value length:" + objectValueSize + "
-			// kb");
-			if (tempFile.delete()) {
-				// System.out.println("Info:Temp file Deleted Successfully");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return objectValueSize;
-	}
-
-	private double getSoureFileSize() {
-		long fileSize = 0;
-		try {
-			File soureFi = new File(this.currentPath);
-			fileSize = (((soureFi.length() / 1024) / 1024) / 1024);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return fileSize;
-	}
-
-	public  String doCreateAction(String key, ObjectNode values, String... tot) {
+	public String doCreateAction(String key, ObjectNode values, String... tot) {
 		JsonNode root = null;
 		String log = "";
 		try {
@@ -176,30 +109,99 @@ public class JsonStorage {
 		return cal;
 	}
 
-	private String checkAndCreateFilePath(String optionalPath) {
-		
+	private static Boolean checkTTLProperty(String key, JsonNode root) throws ParseException {
+		Boolean status = false;
+		String ttlValue = ((ObjectNode) root).path(key).path("TTL").asText();
+		SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM d HH:mm:ss zzz yyyy");
+		Date ttl = formatter.parse(ttlValue);
+		Date currentDate = new Date();
+		if (ttl.compareTo(currentDate) < 0) {
+			status = true;
+		}
+		return status;
+	}
+
+	public synchronized String delete(String key) {
+		String log = "";
 		try {
-			if (!optionalPath.equals("")) {
-				File path = new File(optionalPath);
-				if (path.exists()) {
-					this.currentPath = optionalPath + "\\local_storage.json";
-				} else {
-					System.out.println("Error: Please Check Your Folder Path: "+optionalPath);
-				}
-			} else {
-				File newFileObj = new File(defaultFilePath);
-				if (newFileObj.exists()) {
-					this.currentPath  = defaultFilePath + "\\local_storage.json";
-				} else {
-					if (newFileObj.mkdir()) {
-					 this.currentPath  = defaultFilePath + "\\local_storage.json";
+			if (!this.currentPath.equals("")) {
+				File sourceFile = new File(this.currentPath);
+				if (sourceFile.exists()) {
+					JsonNode root = mapper.readTree(sourceFile);
+					if (root != null) {
+						if (root.get(key) != null) {
+							if (checkTTLProperty(key, root)) {
+								log = "Error :This key has expired,this key will no longer be available to Delete operations";
+							} else {
+								((ObjectNode) root).remove(key);
+								log = "Info: " + key + " key & values has been deleted successfully";
+								mapper.writeValue(sourceFile, root);
+							}
+						} else {
+							log = "Info: Key is not found";
+						}
 					}
 				}
 			}
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return currentPath;
+		return log;
+	}
+
+	public synchronized String read(String key) {
+		ArrayList<Object> listobj = new ArrayList<>();
+		try {
+			if (!this.currentPath.equals("")) {
+				File sourceFile = new File(this.currentPath);
+				if (sourceFile.exists()) {
+					JsonNode root = mapper.readTree(sourceFile);
+					if (root != null) {
+						if (root.get(key) != null) {
+							if (checkTTLProperty(key, root)) {
+								listobj.add(
+										"Error :This key has expired,the key will no longer be available to Read operations");
+							} else {
+								listobj.add(root.path(key));
+							}
+						} else {
+							listobj.add("Info : Key is not found");
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return listobj.get(0).toString();
+	}
+
+	private static double getObjectSize(ObjectNode values) {
+		double objectValueSize = 0.00;
+		try {
+			File tempFile = File.createTempFile("fw-tempFile", ".tmp");
+			mapper.writeValue(tempFile, values);
+			double kilobytes = ((double) tempFile.length() / 1024);
+			objectValueSize = Math.round(kilobytes);
+			// System.out.println("Info: value length:" + objectValueSize + "
+			// kb");
+			if (tempFile.delete()) {
+				// System.out.println("Info:Temp file Deleted Successfully");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return objectValueSize;
+	}
+
+	private double getSoureFileSize() {
+		long fileSize = 0;
+		try {
+			File soureFi = new File(this.currentPath);
+			fileSize = (((soureFi.length() / 1024) / 1024) / 1024);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return fileSize;
 	}
 }
